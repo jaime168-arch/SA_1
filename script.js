e9c1eae0a0362e06d19f80abf89fbf0256b216b5
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-05-04 11:23:18 -0300

    add- login

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const loginForm = document.getElementById('loginForm');
  if (loginForm) {
     loginForm.addEventListener('submit', function(e) {
+        
+    }
         e.preventDefault(); 
-
+ 
         const user = document.getElementById('usuario').value.trim();
         const pass = document.getElementById('senha').value.trim();
         const msg = document.getElementById('msgErro');
