e2ece6239b6567895a155e64435c8b73fa67704e
MariaLoretto <maria_santos83@estudante.sesisenai.org.br>
2026-05-04 12:51:46 -0300

    add

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -12,32 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     cadastroForm.addEventListener('submit', (event) => {
-    
-        const senha1 = document.getElementById('senha1').value;
-        const senha2 = document.getElementById('senha2').value;
+ 
+        const s1 = document.getElementById('senha1').value;
+        const s2 = document.getElementById('senha2').value;
 
-
-        if (senha1 !== senha2) {
-            event.preventDefault();
-            alert('As senhas não coincidem!');
-            return;
+        if (s1 !== s2) {
+            event.preventDefault(); 
+            alert('Erro: As senhas não coincidem! Tente novamente.');
+            return; 
         }
 
-        if (senha1.length < 6) {
+       
+        if (s1.length < 6) {
             event.preventDefault();
             alert('A senha deve ter pelo menos 6 caracteres.');
             return;
         }
 
-        const usuario = {
-            nome: document.getElementById('nome').value.trim(),
-            email: document.getElementById('email').value.trim(),
-            telefone: telefoneInput.value.trim(),
-            senha: senha1
-        };
-
-        localStorage.setItem('usuarioCadastrado', JSON.stringify(usuario));
         alert('Cadastro realizado com sucesso!');
-        window.location.href = "index.html";
     });
 });
